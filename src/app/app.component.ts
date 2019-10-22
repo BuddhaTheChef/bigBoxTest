8bf5588625b07daa1521981a9b85b7b7daba05a2
Aj Wietecha II <ajwietechaii@yahoo.com>
2019-10-21 17:29:27 -0700

    added in a chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as CanvasJS from '../canvasjs.min';
+//var CanvasJS = require('./canvasjs.min');
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'bigBoxTest';
+ 
+export class AppComponent implements OnInit {
+	ngOnInit() {
+	let chart = new CanvasJS.Chart("chartContainer", {
+		theme: "light2",
+		animationEnabled: true,
+		exportEnabled: true,
+		title:{
+			text: "Monthly Expense"
+		},
+		data: [{
+			type: "pie",
+			showInLegend: true,
+			toolTipContent: "<b>{name}</b>: ${y} (#percent%)",
+			indexLabel: "{name} - #percent%",
+			dataPoints: [
+				{ y: 450, name: "Food" },
+				{ y: 120, name: "Insurance" },
+				{ y: 300, name: "Traveling" },
+				{ y: 800, name: "Housing" },
+				{ y: 150, name: "Education" },
+				{ y: 150, name: "Shopping"},
+				{ y: 250, name: "Others" }
+			]
+		}]
+	});
+		
+	chart.render();
+    }
 }
